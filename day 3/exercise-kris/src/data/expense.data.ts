8dbbad1f16c230241adb2f5b5f7d8489b7c147a5
Kristanto Nugraha <33407224+Isoggi@users.noreply.github.com>
2024-08-13 15:12:30 +0700

    Day 3 Exercise

--- day 3/exercise-kris/src/data/expense.data.ts
+++ day 3/exercise-kris/src/data/expense.data.ts
@@ -1,31 +1,36 @@
 import fs from "fs";
 import { IExpense, IExpenses } from "../interfaces/expense.interface";
 
-const path = "./data/data.json";
-const logPath = "./datalog.txt";
+const path = "./src/data/data.json";
+const logPath = "./src/data/datalog.txt";
 
 const getData = () => {
   return JSON.parse(fs.readFileSync(path, "utf8"));
 };
 
 const writeData = (data: any) => {
-  fs.writeFileSync(path, data);
+  fs.writeFileSync(path, JSON.stringify(data));
 };
 
 const writeLog = (message: string = "") => {
   const log = fs.readFileSync(logPath, "utf-8");
-  fs.writeFileSync(logPath, log + " \n" + new Date() + " " + message);
+  fs.writeFileSync(
+    logPath,
+    JSON.stringify(log + " \n" + new Date() + " \n" + message)
+  );
 };
 
 const getExpenses = (id?: number) => {
   if (id) {
-    return getData().expenses.find((exp: IExpense) => exp.id === id);
+    return getData().expenses.find((exp: IExpense) => exp.id === id) ?? {};
   }
   return getData().expenses as IExpenses;
 };
 
 const addExpense = (data: IExpense) => {
+  console.log(getData());
   const expenses = getData().expenses;
+  console.log(getData(), expenses);
   data.id = expenses.length + 1;
   expenses.push(data);
   writeData({ expenses });
@@ -51,9 +56,51 @@ const deleteExpense = (id: number) => {
   return { id };
 };
 
+type totalParams = {
+  type?: "date" | "category";
+  params?: any;
+};
+
+const totalExpense = ({ type, params }: totalParams = {}) => {
+  const expenses = getData().expenses;
+  let totalExpense = 0;
+  expenses
+    .filter(
+      (x: IExpense) =>
+        x.type === "expense" &&
+        (type
+          ? type === "date"
+            ? x.date >= params.start && x.date <= params.end
+            : x.category === params
+          : true)
+    )
+    .map((exp: IExpense) => {
+      totalExpense += exp.nominal;
+    });
+  return totalExpense;
+};
+
+const calcExpense = () => {
+  const expenses = getData().expenses;
+  let totalIncome = 0;
+  expenses
+    .filter((x: IExpense) => x.type === "income")
+    .map((exp: IExpense) => {
+      totalIncome += exp.nominal;
+    });
+  let totalExpense = 0;
+  expenses
+    .filter((x: IExpense) => x.type === "expense")
+    .map((exp: IExpense) => {
+      totalExpense += exp.nominal;
+    });
+  return totalIncome - totalExpense;
+};
+
 export const expenseData = {
   getExpenses,
   addExpense,
   updateExpense,
   deleteExpense,
+  totalExpense,
 };
--- day 3/exercise-kris/src/routes/expense.route.ts
+++ day 3/exercise-kris/src/routes/expense.route.ts
@@ -1,6 +1,7 @@
 import { Router, Response, Request } from "express";
 import fs from "fs";
 import { expenseData } from "../data/expense.data";
+import { IExpense } from "../interfaces/expense.interface";
 export const expenseRouter = Router();
 
 expenseRouter.get("/", (req: Request, res: Response) => {
@@ -8,33 +9,35 @@ expenseRouter.get("/", (req: Request, res: Response) => {
   res.send({
     success: true,
     data: result,
-    message: "Expense Kris: " + result.length,
-  });
-});
-
-expenseRouter.get("/:id", (req: Request, res: Response) => {
-  const result = expenseData.getExpenses(Number(req.params.id));
-  res.send({
-    success: true,
-    data: result,
-    message: "Expense Kris" + result.title,
+    message: "Expense List: " + result.length,
   });
 });
 
 expenseRouter.post("/", (req: Request, res: Response) => {
-  const result = expenseData.addExpense(req.body);
+  console.log(req.body);
+  const result = expenseData.addExpense(req.body as IExpense);
   res.status(201).send({
     success: true,
     data: result,
-    message: "Post Expense",
+    message: "Post Expense: " + result.title,
   });
 });
 
 expenseRouter.patch("/:id", (req: Request, res: Response) => {
+  console.log(req.body);
   const result = expenseData.updateExpense(req.body);
   res.send({
     success: true,
     message: "Patch Expense: " + result,
+  });
+});
+
+expenseRouter.put("/:id", (req: Request, res: Response) => {
+  console.log(req.body);
+  const result = expenseData.updateExpense(req.body);
+  res.send({
+    success: true,
+    message: "Put Expense: " + JSON.stringify(result),
   });
 });
 
@@ -43,5 +46,28 @@ expenseRouter.delete("/:id", (req: Request, res: Response) => {
   res.send({
     success: true,
     message: "Delete Expense: " + result,
+  });
+});
+
+expenseRouter.get("/total", (req: Request, res: Response) => {
+  const types = req.query.type as { type: "date" | "category" }["type"];
+  const params =
+    types === "date"
+      ? { start: req.query.start, end: req.query.end }
+      : req.query.category;
+  const result = expenseData.totalExpense({ type: types, params });
+  res.send({
+    success: true,
+    data: result,
+    message: "Total Expense: " + result,
+  });
+});
+
+expenseRouter.get("/:id", (req: Request, res: Response) => {
+  const result = expenseData.getExpenses(Number(req.params.id));
+  res.send({
+    success: true,
+    data: result,
+    message: "Get Expense: " + (result.title ?? "Empty"),
   });
 });
