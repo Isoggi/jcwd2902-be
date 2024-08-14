56b91e5c0dfbc60eb199c76e7b063095d0b0fbbe
Kristanto Nugraha <33407224+Isoggi@users.noreply.github.com>
2024-08-14 16:23:31 +0700

    update services

--- day 4/api-mysql/src/services/student.service.ts
+++ day 4/api-mysql/src/services/student.service.ts
@@ -1,14 +1,19 @@
+import { IStudent } from "../interfaces/student.interface";
 import db from "../libs/mysql.lib";
 export class StudentService {
+  private static async run(query: string) {
+    const [result] = await db.promise().query(query);
+    return result;
+  }
   public static async getAllService() {
     const sql = "SELECT * FROM student";
-    const [result] = await db.promise().query(sql);
+    const result = (await this.run(sql)) as IStudent[];
     return result;
   }
   public static async getByIdService(id: number) {
     const sql = `SELECT * FROM student WHERE id = ${id} LIMIT 0,1`;
-    const result = await db.promise().query(sql);
-    return result;
+    const result = (await this.run(sql)) as IStudent[];
+    return result[0];
   }
   public createService() {}
   public updateService() {}
