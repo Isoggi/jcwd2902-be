a9bca4c2cf32bc002b99540d76c14c6ef4c9d694
Kristanto Nugraha <33407224+Isoggi@users.noreply.github.com>
2024-08-19 16:15:56 +0700

    Update SQL day 6

--- day 5/exercise-kris/4.sql
+++ day 5/exercise-kris/4.sql
@@ -14,4 +14,9 @@ SELECT actor.first_name, actor.last_name, COUNT(film_actor.film_id) FROM actor J
 
 SELECT title, description, `length`, rating, special_features from film WHERE special_features LIKE '%Deleted Scenes%' AND special_features LIKE '%Behind the Scenes%' ORDER BY `length` DESC; 
 
-SELECT * from country JOIN customer WHERE country.country_id = customer.;
+select C.country, count(cu.customer_id) total_inactive_customer  from country C 
+join city ct on ct.country_id = c.country_id
+join address a on a.city_id = ct.city_id
+join customer cu on cu.address_id = a.address_id and cu.active = 0
+group by c.country
+order by total_inactive_customer desc;
---
+++ day 6/exercise-kris/1.sql
@@ -0,0 +1,50 @@
+USE jcwd2902_join_table;
+
+create table Member (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    email varchar(32) NOT NULL,
+    phone_number varchar(32) NOT NULL,
+    first_name varchar(32) NOT NULL,
+    last_name varchar(32),
+    registered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE)
+
+
+create Table if not exists Author (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    fullname varchar(32) NOT NULL,
+    firstname varchar(32) NOT NULL,
+    lastname varchar(32) NOT NULL,
+    UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE) 
+    ENGINE = InnoDB
+
+create Table IF NOT EXISTS Books (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY,
+ bookname varchar(32) NOT NULL,
+ author_id INTEGER NOT NULL,
+ isbn VARCHAR(16) NOT NULL,
+ revision_no VARCHAR(32),
+ year_of_publication INTEGER NOT NULL,
+ FOREIGN KEY (author_id) REFERENCES Author(id),
+UNIQUE INDEX `id_UNIQUE` (id ASC) VISIBLE)
+ENGINE = InnoDB;
+
+create Table BookTransactions(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    book_id INTEGER NOT NULL,
+    member_id INTEGER NOT NULL,
+    borrowed_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    returned_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (book_id) REFERENCES Books(id),
+    FOREIGN KEY (member_id) REFERENCES Member(id)
+)
+
+create Table BookStocks(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    book_id INTEGER NOT NULL,
+    stock INT DEFAULT 0
+
+)
+
+
